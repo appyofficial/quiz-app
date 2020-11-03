968bd04ad71ec5cd1d3863dbdd40ba4dfe523a82
Appy Sharma <appy70890@gmail.com>
2020-11-03 16:35:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import reducer, { initialState } from "./context/reducer";
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
